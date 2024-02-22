22b87e3e1f4d8bba1f64d9d1f3de56db5d128e88
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-02-22 11:35:30 +0100

    added arguments to Tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,5 +1,27 @@
 console.log('tamagotchi file is loaded');
 
 class Tamagotchi {
-  // code of the class goes here
+
+  foodInTummy;
+  restedness;
+  health;
+
+    constructor(name, type) {
+      this.foodInTummy = 10;
+      this.restedness = 10;
+      this.health = 10;
+      this.name = name;
+      this.creatureType = type;
+    }
+
+    cry() {
+      this.foodInTummy -= 1;
+      console.log(this.name + " a " + this.foodInTummy + " points de nourriture. WHAOOOOOOOO ! ! !");
+    }
 }
+
+const tamagotchi_one = new Tamagotchi("Lucas", "Serpent à sonette");
+const tamagotchi_two = new Tamagotchi("Katchweene", "Pokemon");
+
+tamagotchi_one.cry();
+tamagotchi_two.cry();
